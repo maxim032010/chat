d7481409561200ea7c06772c78c3cff8ed0d1795
maxim032010 <mbannikow20@gmail.com>
2023-12-12 21:14:01 +0100

    Upload,12.DEZ.23

---
+++ backend/index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios"); // Füge diese Zeile hinzu
+
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: true }));
+
+app.post("/authenticate", async (req, res) => {
+  const { username } = req.body;
+  try {
+    const r = await axios.put(
+      'https://api.chatengine.io/users/',
+      { username: username, secret: username, first_name: username },
+      { headers: { "private-key": "6b8a7ae0-8c7d-47f2-b11e-4867fd86b84d" } }
+    );
+    return res.status(r.status).json(r.data);
+  } catch (e) {
+    return res.status(e.response.status).json(e.response.data);
+  }
+});
+
+app.listen(3001);
